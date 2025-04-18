56d15293fab2ce1be79cf4cf25c5cdfbcf64bdbb
hongnhut2404 <nhutth2404@gmail.com>
2025-04-18 14:55:20 +0700

    add files for optimization

---
+++ optimize/optimize.py
@@ -0,0 +1,43 @@
+import optuna
+import warnings
+import numpy as np
+from backtesting.backtesting import *
+
+# warnings.filterwarnings("ignore")
+
+def objectives(trial):
+    period_rsi = trial.suggest_int("period_rsi", 5, 20)
+    period_bb = trial.suggest_int("period_bb", 10, 100)
+    in_sample_size = 0.8
+    risk_per_trade = trial.suggest_float("risk_per_trade", 0.01, 0.5)
+    rsi_oversold = trial.suggest_int("rsi_oversold", 5, 30)
+    rsi_overbought = trial.suggest_int("rsi_overbought", 70, 90)
+    rsi_extreme_overbought = trial.suggest_int("rsi_extreme_overbought", 80, 95)
+
+    backtest = Backtesting(
+        period_rsi,
+        period_bb,
+        in_sample_size,
+        risk_per_trade,
+        rsi_oversold,
+        rsi_overbought,
+        rsi_extreme_overbought
+    )
+
+    backtest.initiate_data()
+    backtest.apply_indicators()
+
+    sharpe_ratio = backtest.run_backtest(returns_sharpe=True)
+
+    if sharpe_ratio is None or np.isnan(sharpe_ratio):
+        return float("-inf")
+    
+    return sharpe_ratio
+
+if __name__ == "__main__":
+    study = optuna.create_study(direction="maximize")
+    study.optimize(objectives, n_trials=50)  
+
+    print("Best hyperparameters:")
+    print(study.best_params)
+    print(f"Best Sharpe Ratio: {study.best_value}")
